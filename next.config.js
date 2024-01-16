083fe8aa5daeb34fc512fde3a4f95794895ef861
dkdkv <dulustan1996@gmail.com>
2024-01-16 14:25:35 +0900

    feat: configure image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'images.unsplash.com',
+                protocol: 'https',
+                pathname: '/photo-:id',
+            },
+        ],
+    }
+};
 
 module.exports = nextConfig;
